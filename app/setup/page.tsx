76f7d0ca532949c4a5f40d2b1fdfb961cb06a104
hobbyistmaster <hobbyistmaster@gmail.com>
2025-11-29 15:18:37 -0600

    Create page.tsx

---
+++ app/setup/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function SetupPage() {
+  return (
+    <div className="text-white p-8">
+      <h1 className="text-3xl font-bold mb-4">Create New Game</h1>
+      <p>Setup screen will go here.</p>
+    </div>
+  );
+}
